943bf9c0ac17f287d85afb798bf59bc549f9ef17
Martinatjie <martinabrockmann@gmail.com>
2022-12-04 11:19:56 +0200

    Part 1

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -21,30 +21,15 @@ Console.ReadLine();
 
 static bool CompareCleanupSpots(string input)
 {
+    var fullyContains = false;
     var splitStrings = input.Split(',', '-');
     var splitNumbers = Array.ConvertAll(splitStrings, int.Parse);
 
-    //create the strings
+    var aContainsB = (splitNumbers[2] >= splitNumbers[0]) && (splitNumbers[3] <= splitNumbers[1]);
+    var bContaisnA = (splitNumbers[0] >= splitNumbers[2]) && (splitNumbers[1] <= splitNumbers[3]);
 
-    var elfRangeA = "";
-    var elfRangeB = "";
+    //check which range is the highest/longest
+    if (aContainsB || bContaisnA) { fullyContains = true; };
 
-    for (var a = splitNumbers[0]; a <= splitNumbers[1]; a++)
-    {
-        elfRangeA = elfRangeA + '.' + a;
-    }
-
-    for (var b = splitNumbers[2]; b <= splitNumbers[3]; b++)
-    {
-        elfRangeB = elfRangeB + '.' + b;
-    }
-
-    string[] array = new string[2] { elfRangeA, elfRangeB };
-
-    var fullyContains = array[0].Contains(array[1]) || array[1].Contains(array[0]);
-    if (fullyContains)
-    {
-        Console.WriteLine($"{elfRangeA} \r\n + \r\n {elfRangeB} \r\n\r\n");
-    }
     return fullyContains;
 }
