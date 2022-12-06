22d625749cef7579ffc1b2fdc38b20a04ffd2e68
Martinatjie <martinabrockmann@gmail.com>
2022-12-06 08:52:25 +0200

    day 6 done and dusted

---
+++ Day6/Program.cs
@@ -0,0 +1,31 @@
+﻿
+//read the file, 4 characters at a time
+var characters = File.ReadAllText(@"Characters.txt");
+
+var take = 14;
+var skip = 0;
+var distinct = false;
+var characterPosition = 0;
+
+while (!distinct)
+{
+    var chosenCharacters = characters.Skip(skip).Take(take);
+    var distinctCount = chosenCharacters.Distinct().Count();
+
+    if (distinctCount != take)
+    {
+        //not all are distinct, move on and take next characters
+        skip = skip + 1;
+    }
+    else
+    {
+        //all characters are distinct
+        characterPosition = skip + take;
+
+        distinct = true;
+    }
+}
+
+Console.WriteLine(characterPosition);
+
+Console.ReadLine();
